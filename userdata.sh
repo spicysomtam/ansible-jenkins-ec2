d56427fd48adc9a1f2f30bcd047023e762246898
Alastair Munro <al.munro@freedom-finance.com>
2019-01-07 11:29:02 +0000

    Added swapfile to userdata.

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -xv
 exec > /tmp/userdata.log 2>&1
 
+# Add some swap so system does not run out of memory:
+dd if=/dev/zero of=/swapfile bs=1G count=2
+chmod 0600 /swapfile
+mkswap /swapfile
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+swapon -a
+
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y java-1.8.0-openjdk jenkins
